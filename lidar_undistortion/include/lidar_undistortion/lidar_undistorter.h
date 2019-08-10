a9fdf41d1b2fc0f20c22b67a4cc048494bcecb92
Victor Reijgwart <victor_reijgwart@hotmail.com>
2019-08-10 19:30:09 -0400

    Minor optimizations (3x performance gain)
    Only lookup TFs if the timestamp differs from the previous one.
    Use Eigen::Affine transforms instead of kindr.
    Vectorized transform from odom frame (after undistortion) back to scan frame.

--- lidar_undistortion/include/lidar_undistortion/lidar_undistorter.h
+++ lidar_undistortion/include/lidar_undistortion/lidar_undistorter.h
@@ -1,22 +1,17 @@
 #ifndef LIDAR_UNDISTORTION_LIDAR_UNDISTORTER_H_
 #define LIDAR_UNDISTORTION_LIDAR_UNDISTORTER_H_
 
-#include <ouster_ros/point_os1.h>
-#include <pcl_conversions/pcl_conversions.h>
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
-
 #include <tf2_ros/transform_listener.h>
-
-#include <minkindr_conversions/kindr_msg.h>
-// TODO(victorr): Look into using kindr_tf.h
+#include <Eigen/Eigen>
 
 namespace lidar_undistortion {
 class LidarUndistorter {
  public:
   LidarUndistorter(ros::NodeHandle nh, ros::NodeHandle nh_private);
 
-  void pointcloudCallback(const sensor_msgs::PointCloud2::Ptr &pointcloud_msg);
+  void pointcloudCallback(const sensor_msgs::PointCloud2 &pointcloud_msg);
 
  private:
   ros::NodeHandle nh_;
@@ -27,6 +22,18 @@ class LidarUndistorter {
 
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener tf_listener_;
+
+  // TODO(victorr): Explain why this is here
+  template <typename T>
+  void transformMsgToEigen(const geometry_msgs::Transform &transform_msg,
+                           T &transform) {  // NOLINT
+    transform =
+        Eigen::Translation3f(transform_msg.translation.x,
+                             transform_msg.translation.y,
+                             transform_msg.translation.z) *
+        Eigen::Quaternionf(transform_msg.rotation.w, transform_msg.rotation.x,
+                           transform_msg.rotation.y, transform_msg.rotation.z);
+  }
 };
 }  // namespace lidar_undistortion
 
